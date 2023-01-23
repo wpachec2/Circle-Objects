03f1571bf8cb850aeb13ce59190646cd88b767c0
wpachec2 <fredojpacheco@gmail.com>
2023-01-23 17:02:59 -0500

    Circle Objects

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CircleObjects
+{
+	public class Circle
+	{
+		public double radius { get; set; }
+
+        public Circle(double _radius)
+		{
+			radius = _radius;
+		}
+
+		public double CalculateCircumference()
+		{
+			return 2 * Math.PI * radius;
+		}
+		public string CalculateFormattedCircumference()
+		{
+			return FormattedNumber(CalculateCircumference());
+		}
+
+
+		public double CalculateArea()
+		{
+			return Math.PI * Math.Pow(radius, 2);
+		}
+		public string CalculateFormattedArea()
+		{
+			return FormattedNumber(CalculateArea());
+		}
+
+
+        private string FormattedNumber(double x)
+		{
+			string FormattedArea = Math.Round(x, 2).ToString();
+			return FormattedArea;
+		}
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Metrics;
+using CircleObjects;
+
+int circle = 1;
+
+bool restart = true;
+while (restart)
+{
+    Console.WriteLine("Welcome to the Circle Tester!\n");
+
+    Console.Write("Enter Radius of Circle: ");
+    double value = double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Circle {circle++}:");
+
+    Circle myCircle = new Circle(value);
+    Console.WriteLine($"Circimference: {myCircle.CalculateFormattedCircumference()}");
+    Console.WriteLine($"Area: {myCircle.CalculateFormattedArea()}");
+
+    Console.WriteLine();
+    exitProgram(ref restart);
+}
+Console.WriteLine($"You created {circle - 1} Circle Objects. Goodbye!!");
+
+
+static void exitProgram(ref bool x)
+{
+    while (true)
+    {
+        Console.Write("Continue? (y/n): ");
+        string answer = Console.ReadLine().ToLower().Trim();
+
+        if (answer.Contains('y'))
+        {
+            Console.Clear();
+            break;
+        }
+        else if (answer.Contains('n'))
+        {
+            x = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Entry.");
+        }
+    }
+}
